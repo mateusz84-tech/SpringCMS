3be427a2c1fa958f2bcb583a4a9bbb94d19ee18a
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-05-08 20:05:50 +0200

    Article Controller

---
+++ src/main/java/pl/coderslab/config/ArticleController.java
@@ -0,0 +1,21 @@
+package pl.coderslab.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.domain.dao.ArticleDao;
+
+@Controller
+@RequestMapping("/formArticle")
+public class ArticleController {
+
+    private final Logger log = LoggerFactory.getLogger(ArticleController.class);
+
+    private final ArticleDao articleDao;
+
+    public ArticleController(ArticleDao articleDao) {
+        this.articleDao = articleDao;
+    }
+
+}
