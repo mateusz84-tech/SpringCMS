dc4b5e12c809cb116eb2a0a431cec38ddf9fb843
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-05-08 14:57:08 +0200

    add ViewResolver

--- src/main/java/pl/coderslab/config/WebConfig.java
+++ src/main/java/pl/coderslab/config/WebConfig.java
@@ -1,11 +1,14 @@
 package pl.coderslab.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.nio.charset.Charset;
 import java.util.Arrays;
@@ -20,7 +23,14 @@ public class WebConfig implements WebMvcConfigurer {
         stringConverter.setSupportedMediaTypes(Arrays.asList(new MediaType("text", "plain",
                 Charset.forName("UTF-8"))));
         converters.add(stringConverter);
+    }
 
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
     }
 
 }
